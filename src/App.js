f0a4014159040501c65724b4d50125aa4d54bcbe
girishkuwar <girishkuwar11@gmail.com>
2023-02-10 05:26:21 -0800

    h

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+    <>
+    <div>Hello Address</div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <navbar></navbar>
     </div>
+    </>
   );
 }
 
