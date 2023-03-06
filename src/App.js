6b3c740b25036f1d298b2c869a0ddc2c87757dd4
girishkuwar <girishkuwar11@gmail.com>
2023-03-06 04:17:47 -0800

    started

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
     <>
-    <div>Hello Address</div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload and see magic...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <navbar></navbar>
-    </div>
+    
     </>
   );
 }
